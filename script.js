4b7bfaae3f752ec5f86a2f4bceff838da4134220
VitorMScolari <vmarcals@hotmail.com>
2021-08-05 14:29:30 -0300

    done

---
+++ script.js
@@ -0,0 +1,46 @@
+const people = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+  ];
+
+  const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+  ];
+
+//   const isAdult = people.some(person => {
+//       const currentYear = (new Date()).getFullYear();
+//       if(currentYear - person.year >= 19) {
+//           return true;
+//       }
+//   });
+
+  const isAdults = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+  const allAdults = people.every(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+  const comment = comments.find(comment => {
+      if(comment.id === 823423) {
+          return true;
+      }
+  });
+
+  const comment = comments.find(comment => comment.id === 823423);
+
+  const index = comments.findIndex(comment => comment.id === 823423);
+
+//   
+// comments.splice(index, 1);
+// 
+// removes element and modifies array /\
+
+
+const newComments = [
+    ...comments.slice(0, index),
+    ...comments.slice(index + 1)
+];
